f3546b9a3c9cac3cc08d549a1e94a20578a1b168
BormidaEros <eros.bormida.epet20@gmail.com>
2023-12-13 15:34:54 -0300

    Abm y Escritura en firebase

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,41 @@
 import { useAuth } from "../context/AuthContext";
+import React, { useState } from 'react';
+import 'firebase/firestore';
+import { getFirestore, collection, addDoc } from 'firebase/firestore';
+import { db } from '../firebase.js';
+
 
 export function Home() {
+  const [products, setProducts] = useState([]);
+  const [newProduct, setNewProduct] = useState('');
+  const [selectedProduct, setSelectedProduct] = useState(null);
   const { logout, user } = useAuth();
-
   console.log(user);
+  const addProduct = () => {
+    if (newProduct.trim() !== '') {
+      setProducts([...products, newProduct]);
+      setNewProduct('');
+    }
+  };
+
+  const removeProduct = (index) => {
+    const updatedProducts = [...products];
+    updatedProducts.splice(index, 1);
+    setProducts(updatedProducts);
+  };
+
+  const editProduct = (index) => {
+    setSelectedProduct({ index, name: products[index] });
+  };
+
+  const updateProduct = () => {
+    if (selectedProduct && selectedProduct.name.trim() !== '') {
+      const updatedProducts = [...products];
+      updatedProducts[selectedProduct.index] = selectedProduct.name;
+      setProducts(updatedProducts);
+      setSelectedProduct(null);
+    }
+  };
   const handleLogout = async () => {
     try {
       await logout();
@@ -11,11 +43,67 @@ export function Home() {
       console.error(error.message);
     }
   };
+  const [data, setData] = useState('');
+
+  const handleInputChange = (e) => {
+    setData(e.target.value);
+  };
+
+  const handleSaveData = async () => {
+    try {
+      const data = {
+        dato: data,
+      };
+      const docRef = await addDoc(collection(db, 'poec'), data);
+  
+      console.log('Dato guardado exitosamente en Firestore con ID:', docRef.id);
+    } catch (error) {
+      console.error('Error al guardar el dato:', error);
+    }
+  
+  };
 
   return (
+    <div  className="w-full max-w-xs m-auto text-black">
+    <div >
+      <h2>Manejo de Productos</h2>
+      <ul>
+        {products.map((product, index) => (
+          <li key={index}>
+            {product}
+            <button onClick={() => removeProduct(index)}  className="bg-slate-200 hover:bg-slate-300 rounded py-2 px-4 text-black">Eliminar</button>
+            <button onClick={() => editProduct(index)}  className="bg-slate-200 hover:bg-slate-300 rounded py-2 px-4 text-black">Editar</button>
+          </li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        placeholder="Agregar Producto"
+        value={newProduct}
+        onChange={(e) => setNewProduct(e.target.value)}
+      />
+      <button onClick={addProduct} className="bg-slate-200 hover:bg-slate-300 rounded py-2 px-4 text-black">Agregar Producto</button>
+
+      {selectedProduct && (
+        <div>
+          <input
+            type="text"
+            value={selectedProduct.name}
+            onChange={(e) =>
+              setSelectedProduct({
+                ...selectedProduct,
+                name: e.target.value,
+              })
+            }
+          />
+          <button onClick={updateProduct}>Actualizar el producto</button>
+        </div>
+      )}
+    </div>
+    
     <div className="w-full max-w-xs m-auto text-black">
       <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <p className="text-xl mb-4">welcome {user.displayName || user.email}</p>
+        <p className="text-xl mb-4"> Usuario: {user.displayName || user.email}</p>
         <button
           className="bg-slate-200 hover:bg-slate-300 rounded py-2 px-4 text-black"
           onClick={handleLogout}
@@ -24,5 +112,17 @@ export function Home() {
         </button>
       </div>
     </div>
+    <div>
+      <h2>Enviar Datos a Firebase</h2>
+      <input
+        type="text"
+        placeholder="Ingrese un dato"
+        value={data}
+        onChange={handleInputChange}
+      />
+      <button onClick={handleSaveData}>Guardar en Firebase</button>
+    </div>
+   </div>
+    
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from 'firebase/firestore';
+import 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyBRyvmiViBlkg3_ZlFVB6VeSwFS4ERiK5g",
@@ -13,6 +15,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 const auth = getAuth(app);
-
-export { app, auth };
+export { app, db, auth}; 
